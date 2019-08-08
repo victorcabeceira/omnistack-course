14449d1906127125dfbcd6bd9734289c52c01d69
victorcabeceira <victorfgcabeceira@gmail.com>
2019-08-07 23:43:31 -0300

    Adding Routes and Login component

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import logo from '../assets/logo.svg';
+
+import './Login.css';
+
+const Login = () => {
+  return (
+    <div className='login-container'>
+      <form>
+        <img src={logo} alt='Tindev' />
+        <input
+          placeholder='Digite seu usuário no Github'
+        />
+        <button type='submit'>Enviar</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
---
+++ frontend/src/pages/Main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Main = () => {
+  return <div>Main page</div>;
+};
+
+export default Main;
